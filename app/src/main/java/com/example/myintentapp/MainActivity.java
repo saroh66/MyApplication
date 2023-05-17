87d7564e70193dffd6dbcbc37d098e9e4d385da6
saroh66 <Sarohhidayah66@gmail.com>
2023-05-17 15:41:04 +0700

    penambahan button

--- app/src/main/java/com/example/myintentapp/MainActivity.java
+++ app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myintentapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        val btnSatu:Button = findViewById(R.id.btn_1)
+        btnSatu.setOnclickListener(this)
+
     }
+
+    override fun
+
+    Onclick(v:View)
+
+    TODO(reason:"Not yet implemented")
+}
+
 }
